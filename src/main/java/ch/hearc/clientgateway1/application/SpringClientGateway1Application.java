3db52bb41b16a682d7aefd0fbb46e921ad1bb4c1
Sébastien Chèvre <seb.chevre@gmail.com>
2019-09-04 07:12:51 +0200

    discovery config

--- src/main/java/ch/hearc/clientgateway1/application/SpringClientGateway1Application.java
+++ src/main/java/ch/hearc/clientgateway1/application/SpringClientGateway1Application.java
@@ -2,7 +2,9 @@ package ch.hearc.clientgateway1.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringClientGateway1Application {
 
